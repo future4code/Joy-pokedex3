fa8222e2b7fb72e486a48c47e3c8b8d8c00b8c72
jao lucas7 <jaolucas7714@gmail.com>
2022-02-10 15:54:14 -0300

    error-page

--- pokedex-labenu/src/pages/error/ErrorPage.js
+++ pokedex-labenu/src/pages/error/ErrorPage.js
@@ -1,13 +1,25 @@
-import { React } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Header from '../../components/header/Header'
+import React from "react";
+import error from "../../assets/error.png"
+import { Typography } from "@material-ui/core";
+import { ErrorImage, ErrorPageContainer } from "./styled";
+import Header from "../../components/header/Header";
 
 const ErrorPage = () => {
-    return(
-        <div>
-            <Header/>
-        </div>
-    )
+  return (
+  <div>
+      <Header/>
+    <ErrorPageContainer>
+      <ErrorImage src={error}/>
+      <Typography 
+        color={'primary'}
+        variant={'h4'}
+        align={'center'}
+      >
+          <p>Error 404 - PAGE NOT FOUND </p>
+      </Typography>
+    </ErrorPageContainer>
+    </div>
+  );
 }
 
-export default ErrorPage 
+export default ErrorPage;
---
+++ pokedex-labenu/src/pages/error/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const ErrorPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 40px;
+  p{
+      margin:20px;
+      color: black;
+      text-transform: uppercase;
+
+  }
+`
+
+export const ErrorImage = styled.img`
+  width: 70vw;
+  max-width: 500px;
+`
